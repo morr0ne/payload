c1a412953b9fb32c4893d593fb3a669b127105bb
Matilde Morrone <contact@morrone.dev>
2022-07-05 17:19:36 +0200

    Updated main comand execution

--- src/cargo/mod.rs
+++ src/cargo/mod.rs
@@ -6,6 +6,8 @@ mod unit_graph;
 mod version;
 
 pub use error::{ParsingError, Result};
+pub use metadata::Metadata;
+pub use unit_graph::UnitGraph;
 pub use version::Version;
 
 pub struct Cargo(Command);
@@ -36,7 +38,30 @@ impl Cargo {
     }
 
     pub fn version(&mut self) -> Result<Version> {
-        std::str::from_utf8(&self.exec()?)?.parse()
+        std::str::from_utf8(&self.arg("-Vv").exec()?)?.parse()
+    }
+
+    /// Just for testing, don't use.
+    #[doc(hidden)]
+    pub fn _build(&mut self) -> Result<UnitGraph> {
+        let stdout = self
+            .arg("+nightly")
+            .arg("build")
+            .arg("-Zunstable-options")
+            .arg("--unit-graph")
+            .exec()?;
+
+        Ok(serde_json::from_slice(&stdout)?)
+    }
+
+    pub fn metadata(&mut self) -> Result<Metadata> {
+        let stdout = self
+            .arg("metadata")
+            .arg("--format-version")
+            .arg("1")
+            .exec()?;
+
+        Ok(serde_json::from_slice(&stdout)?)
     }
 }
 
