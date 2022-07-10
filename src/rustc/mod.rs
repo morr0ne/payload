8b560bbb2a25d9f5ff184141d0cd81c9783095f9
Matilde Morrone <contact@morrone.dev>
2022-07-10 15:25:30 +0200

    Added rustc builder

--- src/rustc/mod.rs
+++ src/rustc/mod.rs
@@ -1,1 +1,20 @@
+use std::path::PathBuf;
+use which::which;
 
+pub struct Rustc {
+    path: PathBuf,
+}
+
+impl Rustc {
+    pub fn new() -> Self {
+        let path = which("cargo").unwrap_or_else(|_| PathBuf::from("cargo"));
+
+        Rustc { path }
+    }
+}
+
+impl Default for Rustc {
+    fn default() -> Self {
+        Self::new()
+    }
+}
